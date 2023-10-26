27d9cabebfc23e81f8d233dd5561f5c2bbd82303
Carlo-sh <cruaro@protonmail.com>
2023-10-26 20:09:10 +0200

    Add keyboard support

--- app.js
+++ app.js
@@ -10,10 +10,141 @@ const negative = document.getElementById('minus-btn');
 const point = document.getElementById('point-btn');
 /*-----------------------------------------------*/
 
-// Event Listeners
+// Buttons Functions
 
-clear.addEventListener('click', () => {
+const clearFunc = () => {
 	display.textContent = '0';
+};
+
+const backSpaceFunc = () => {
+	if (display.textContent == '0' || display.textContent === 'Error!') return;
+	if (display.textContent.length === 1) {
+		display.textContent = '0';
+	} else {
+		display.textContent = display.textContent.slice(0, -1);
+	}
+};
+
+const decPointFunc = () => {
+	if (/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1])) return;
+	display.textContent += '.';
+	if (/(?<=\.)\d+(?<=\d)\./.test(display.textContent))
+		display.textContent = display.textContent.slice(0, -1);
+};
+
+const negativeFunc = () => {
+	if (/[+\-x÷]/.test(display.textContent)) return;
+	if (display.textContent === '0') {
+		display.textContent = '-';
+	} else {
+		display.textContent = '-' + display.textContent;
+	}
+};
+
+const equalFunc = () => {
+	if (/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1])) {
+		display.textContent = display.textContent.slice(0, -1);
+		if (display.textContent.match(/\d+/g).length < 2) return;
+	}
+	display.textContent = calculate(display.textContent);
+};
+
+// Keyboard
+
+window.addEventListener('keydown', e => {
+	const btn = document.querySelector(`button[data-key="${e.key}"]`);
+	btn.classList.add('btn-click');
+	btn.classList.remove('h-btn');
+	switch (e.code) {
+		case 'Digit0':
+		case 'Digit1':
+		case 'Digit2':
+		case 'Digit3':
+		case 'Digit4':
+		case 'Digit5':
+		case 'Digit6':
+		case 'Digit8':
+		case 'Digit9':
+			if (display.textContent == '0') {
+				display.textContent = e.key;
+			} else {
+				display.textContent += e.key;
+			}
+			break;
+		case 'Delete':
+			clearFunc();
+			break;
+		case 'Backspace':
+			backSpaceFunc();
+			break;
+		case 'Period':
+			decPointFunc();
+			break;
+		case 'BracketRight':
+			if (
+				/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1]) ||
+				display.textContent === '0'
+			)
+				return;
+			if (e.key === '*') {
+				display.textContent += 'x';
+			} else {
+				display.textContent += '+';
+			}
+			break;
+		case 'Slash':
+			if (e.key === '_') {
+				negativeFunc();
+			} else {
+				if (
+					/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1]) ||
+					display.textContent === '0'
+				)
+					return;
+				display.textContent += '-';
+			}
+			break;
+		case 'Digit7':
+			if (e.key === '7') {
+				if (display.textContent == '0') {
+					display.textContent = e.key;
+				} else {
+					display.textContent += e.key;
+				}
+				break;
+			} else {
+				if (
+					/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1]) ||
+					display.textContent === '0'
+				)
+					return;
+				display.textContent += '÷';
+			}
+			break;
+		case 'Enter':
+			equalFunc();
+			break;
+		default:
+			return;
+	}
+});
+
+window.addEventListener('keyup', e => {
+	const btn = document.querySelector(`button[data-key="${e.key}"]`);
+	btn.classList.remove('btn-click');
+	btn.classList.add('h-btn');
+});
+
+// Mouse
+buttons.forEach(b => {
+	b.addEventListener('mousedown', () => {
+		b.classList.add('btn-click');
+		b.classList.remove('h-btn');
+	});
+	b.addEventListener('mouseup', () => {
+		b.classList.remove('btn-click');
+		b.classList.add('h-btn');
+	});
 });
 
 numBtns.forEach(b => {
@@ -28,55 +159,17 @@ numBtns.forEach(b => {
 
 opBtns.forEach(b => {
 	b.addEventListener('click', () => {
-		if (/[+\-*÷.]/.test(display.textContent[display.textContent.length - 1])) return;
+		if (/[+\-x÷.]/.test(display.textContent[display.textContent.length - 1])) return;
 		if (display.textContent === '0') return;
 		display.textContent += b.textContent;
 	});
 });
 
-buttons.forEach(b => {
-	b.addEventListener('mousedown', () => {
-		b.classList.add('btn-click');
-		b.classList.remove('h-btn');
-	});
-	b.addEventListener('mouseup', () => {
-		b.classList.remove('btn-click');
-		b.classList.add('h-btn');
-	});
-});
-
-backSpace.addEventListener('click', () => {
-	if (display.textContent == '0' || display.textContent === 'Error!') return;
-	if (display.textContent.length === 1) {
-		display.textContent = '0';
-	} else {
-		display.textContent = display.textContent.slice(0, -1);
-	}
-});
-
-negative.addEventListener('click', () => {
-	if (/[+\-*÷]/.test(display.textContent)) return;
-	if (display.textContent === '0') {
-		display.textContent = '-';
-	} else {
-		display.textContent = '-' + display.textContent;
-	}
-});
-
-point.addEventListener('click', () => {
-	if (/[+\-*÷.]/.test(display.textContent[display.textContent.length - 1])) return;
-	display.textContent += '.';
-	if (/(?<=\.)\d+(?<=\d)\./.test(display.textContent))
-		display.textContent = display.textContent.slice(0, -1);
-});
-
-equal.addEventListener('click', () => {
-	if (/[+\-*÷.]/.test(display.textContent[display.textContent.length - 1])) {
-		display.textContent = display.textContent.slice(0, -1);
-		if (display.textContent.match(/\d+/g).length < 2) return;
-	}
-	display.textContent = calculate(display.textContent);
-});
+clear.addEventListener('click', clearFunc);
+backSpace.addEventListener('click', backSpaceFunc);
+point.addEventListener('click', decPointFunc);
+negative.addEventListener('click', negativeFunc);
+equal.addEventListener('click', equalFunc);
 
 // Operations
 
